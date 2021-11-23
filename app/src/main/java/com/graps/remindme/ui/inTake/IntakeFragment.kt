3b57757bca8910b9c275de2bd534c0ba81d558d9
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-23 19:49:04 +0300

    refactor intake fragment class

--- app/src/main/java/com/graps/remindme/ui/inTake/IntakeFragment.kt
+++ app/src/main/java/com/graps/remindme/ui/inTake/IntakeFragment.kt
@@ -2,18 +2,18 @@ package com.graps.remindme.ui.inTake
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.activityViewModels
 import com.graps.remindme.R
 import com.graps.remindme.databinding.FragmentIntakeBinding
 import com.graps.remindme.ui.base.BaseFragment
-import com.graps.remindme.ui.base.BaseViewModel
 
-class IntakeFragment: BaseFragment<FragmentIntakeBinding>(R.layout.fragment_intake) {
-    override val viewModel: BaseViewModel
-        get() = TODO("Not yet implemented")
-    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentIntakeBinding
-        get() = TODO("Not yet implemented")
+class IntakeFragment : BaseFragment<FragmentIntakeBinding>(R.layout.fragment_intake) {
+    override val viewModel by activityViewModels<IntakeViewModel>()
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentIntakeBinding =
+        DataBindingUtil::inflate
 
     override fun setupView() {
-        TODO("Not yet implemented")
+
     }
 }
