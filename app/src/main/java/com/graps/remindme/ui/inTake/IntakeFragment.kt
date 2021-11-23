b8c6c315c036a446af7f650425b8c3af133922d9
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-23 19:46:34 +0300

    add intake fragment

---
+++ app/src/main/java/com/graps/remindme/ui/inTake/IntakeFragment.kt
@@ -0,0 +1,19 @@
+package com.graps.remindme.ui.inTake
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.graps.remindme.R
+import com.graps.remindme.databinding.FragmentIntakeBinding
+import com.graps.remindme.ui.base.BaseFragment
+import com.graps.remindme.ui.base.BaseViewModel
+
+class IntakeFragment: BaseFragment<FragmentIntakeBinding>(R.layout.fragment_intake) {
+    override val viewModel: BaseViewModel
+        get() = TODO("Not yet implemented")
+    override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> FragmentIntakeBinding
+        get() = TODO("Not yet implemented")
+
+    override fun setupView() {
+        TODO("Not yet implemented")
+    }
+}
