11517465a3fc9e103995eca1ad23b4c5484cbea8
Amnah Ali <amnah1li@gmail.com>
2021-11-23 16:48:02 +0300

    refactor main activity to remind me activity

--- app/src/main/java/com/graps/remindme/ui/RemindMeActivity.kt
+++ app/src/main/java/com/graps/remindme/ui/RemindMeActivity.kt
@@ -4,9 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.graps.remindme.R
 
-class MainActivity : AppCompatActivity() {
+class RemindMeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_remind_me)
     }
 }
