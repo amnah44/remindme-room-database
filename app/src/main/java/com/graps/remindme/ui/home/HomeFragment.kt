1e4fe7463ceec63eea76cc7dd5907c5f72efe362
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-23 19:42:39 +0300

    init home view model in home fragment

--- app/src/main/java/com/graps/remindme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/graps/remindme/ui/home/HomeFragment.kt
@@ -1,19 +1,15 @@
 package com.graps.remindme.ui.home
 
-import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
-import androidx.lifecycle.ViewModel
+import androidx.fragment.app.activityViewModels
 import com.graps.remindme.R
 import com.graps.remindme.databinding.FragmentHomeBinding
 import com.graps.remindme.ui.base.BaseFragment
 
 class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
-    override val viewModel: ViewModel
-        get() = TODO("Not yet implemented")
+    override val viewModel by activityViewModels<HomeViewModel>()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) ->
     FragmentHomeBinding = DataBindingUtil::inflate
 
