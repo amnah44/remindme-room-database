d05c809fb4941123cdf3aa0adf6d0305a4169066
Rafal A <rafal.akeel99@gmail.com>
2021-11-23 15:31:41 +0300

    add base fragment

---
+++ app/src/main/java/com/graps/remindme/BaseFragment.kt
@@ -0,0 +1,38 @@
+package com.graps.remindme
+
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.ViewDataBinding
+import androidx.fragment.app.Fragment
+
+abstract class BaseFragment<VDB : ViewDataBinding>(private val fragmentLayoutId: Int) : Fragment() {
+
+    abstract val viewModel: BaseViewModel
+
+    private lateinit var _binding: VDB
+    val binding: VDB get() = _binding
+    abstract val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) -> VDB
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = bindingInflater(inflater, fragmentLayoutId, container, false)
+        _binding.setVariable(BR.viewModel, viewModel)
+        _binding.lifecycleOwner = this
+        return binding.root
+    }
+
+    //test
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupView()
+    }
+
+    abstract fun setupView()
+}
