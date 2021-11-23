812a1015e82b4cfd7ad6f87457627d1e77f2ff2f
Amnah Ali <amnah1li@gmail.com>
2021-11-23 20:08:16 +0300

    command unusable line in base fragment

--- app/src/main/java/com/graps/remindme/ui/base/BaseFragment.kt
+++ app/src/main/java/com/graps/remindme/ui/base/BaseFragment.kt
@@ -6,8 +6,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.ViewDataBinding
+import androidx.databinding.library.baseAdapters.BR
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModel
 
 abstract class BaseFragment<VDB : ViewDataBinding>(private val fragmentLayoutId: Int) : Fragment() {
 
