dc3aae1662fcad8c65d6d4df1c7caad80d2ffa52
Rafal A <rafal.akeel99@gmail.com>
2021-11-23 21:45:00 +0300

    add update function

--- app/src/main/java/com/graps/remindme/data/database/ReminderDao.kt
+++ app/src/main/java/com/graps/remindme/data/database/ReminderDao.kt
@@ -2,6 +2,7 @@ package com.graps.remindme.data.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Update
 import com.graps.remindme.data.Reminder
 
 @Dao
@@ -9,4 +10,7 @@ interface ReminderDao {
 
     @Insert
     fun insertReminder(reminder:Reminder)
+
+    @Update
+    fun updateReminder(reminder:Reminder)
 }
