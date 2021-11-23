7c7330e98355a49bf8ab9c1590b375efe5e4ed46
Rafal A <rafal.akeel99@gmail.com>
2021-11-23 21:44:37 +0300

    add insert function

--- app/src/main/java/com/graps/remindme/data/database/ReminderDao.kt
+++ app/src/main/java/com/graps/remindme/data/database/ReminderDao.kt
@@ -1,4 +1,12 @@
 package com.graps.remindme.data.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import com.graps.remindme.data.Reminder
+
+@Dao
 interface ReminderDao {
+
+    @Insert
+    fun insertReminder(reminder:Reminder)
 }
