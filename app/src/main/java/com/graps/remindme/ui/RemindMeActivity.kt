8a2d40a3b6b6b76e65b5ae2076442d4e0afb9d62
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-24 16:54:08 +0300

    call get instance in activity

--- app/src/main/java/com/graps/remindme/ui/RemindMeActivity.kt
+++ app/src/main/java/com/graps/remindme/ui/RemindMeActivity.kt
@@ -3,10 +3,12 @@ package com.graps.remindme.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.graps.remindme.R
+import com.graps.remindme.data.database.ReminderDatabase
 
 class RemindMeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_remind_me)
+        ReminderDatabase.getInstance(applicationContext)
     }
 }
