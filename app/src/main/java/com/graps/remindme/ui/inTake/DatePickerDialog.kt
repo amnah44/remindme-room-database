c8a0e55d27cdff4515d1cb90813ca9c6b30db29f
basheer <spyderme99100@gmail.com>
2021-11-24 19:21:29 +0300

    add date picker layout

--- app/src/main/java/com/graps/remindme/ui/inTake/DatePickerDialog.kt
+++ app/src/main/java/com/graps/remindme/ui/inTake/DatePickerDialog.kt
@@ -6,19 +6,23 @@ import android.app.Dialog
 import android.content.DialogInterface
 import android.os.Bundle
 import androidx.fragment.app.DialogFragment
+import com.graps.remindme.R
 
 class DatePickerDialog : DialogFragment (){
-
     override fun onCreateDialog(savedInstanceState: Bundle?): Dialog {
         return activity?.let {
             val builder = AlertDialog.Builder(it)
-            builder.setMessage("Choose Date")
-                .setPositiveButton("Set",DialogInterface.OnClickListener{ dialog, id ->
+            val inflater = requireActivity().layoutInflater
 
-                })
-                .setNegativeButton("Cancel",DialogInterface.OnClickListener{ dialog, id ->
+            builder.setView(inflater.inflate(R.layout.date_picker_dialog, null))
+                .setPositiveButton("Set",
+                    DialogInterface.OnClickListener { dialog, id ->
 
-                })
+                    })
+                .setNegativeButton("Cancel",
+                    DialogInterface.OnClickListener { dialog, id ->
+                        getDialog()?.dismiss()
+                    })
             builder.create()
         }?: throw IllegalStateException("Activity cannot be null")
     }
