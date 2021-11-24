f90188445bfeb11045a9d9ee255e7361ad1eccee
Moe Zalzala <Mohammed27Zalzala11@gmail.com>
2021-11-24 13:06:50 +0300

    create BaseDiffutils class

--- app/src/main/java/com/graps/remindme/ui/base/BaseAdapter.kt
+++ app/src/main/java/com/graps/remindme/ui/base/BaseAdapter.kt
@@ -5,6 +5,7 @@ import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.databinding.library.baseAdapters.BR
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
 
---
+++ app/src/main/java/com/graps/remindme/ui/base/BaseDiffutils.kt
@@ -0,0 +1,15 @@
+package com.graps.remindme.ui.base
+
+import androidx.recyclerview.widget.DiffUtil
+
+class BaseDiffutils<T>(private val remindersList: List<T>, private val remindersNewList: List<T>) :
+    DiffUtil.Callback() {
+        override fun getOldListSize() = remindersList.size
+
+        override fun getNewListSize() = remindersNewList.size
+
+        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+            remindersList[oldItemPosition] == remindersNewList[newItemPosition]
+
+        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
+}
