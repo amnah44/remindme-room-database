3283db66e6a6c5719899a2d763d8df8253bb601f
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-24 19:43:24 +0300

    add reminder interaction listener

---
+++ app/src/main/java/com/graps/remindme/ui/home/ReminderAdapter.kt
@@ -0,0 +1,8 @@
+package com.graps.remindme.ui.home
+
+import com.graps.remindme.ui.base.BaseAdapter
+
+class ReminderAdapter {
+}
+
+interface ReminderInteractionListener : BaseAdapter.BaseInteractionListener
