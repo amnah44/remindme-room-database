450218b6317f054ba9d1f72dd57dc3b86da6933c
Anwar Alfarhany <anwar.alfarhany@gmail.com>
2021-11-24 19:44:08 +0300

    add reminder adapter

--- app/src/main/java/com/graps/remindme/ui/home/ReminderAdapter.kt
+++ app/src/main/java/com/graps/remindme/ui/home/ReminderAdapter.kt
@@ -1,8 +1,14 @@
 package com.graps.remindme.ui.home
 
+import com.graps.remindme.R
+import com.graps.remindme.data.Reminder
 import com.graps.remindme.ui.base.BaseAdapter
 
-class ReminderAdapter {
+class ReminderAdapter(
+    items: List<Reminder>,
+    listener: ReminderInteractionListener,
+) : BaseAdapter<Reminder>(items, listener) {
+    override val layoutId: Int = R.layout.item_drug
 }
 
 interface ReminderInteractionListener : BaseAdapter.BaseInteractionListener
