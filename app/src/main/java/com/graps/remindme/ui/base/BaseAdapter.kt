2f2edc53505ab70890eac6630a146a1e59ffaa69
Amnah Ali <amnah1li@gmail.com>
2021-11-24 13:18:53 +0300

    sort classes and remove unusable lines

--- app/src/main/java/com/graps/remindme/ui/base/BaseAdapter.kt
+++ app/src/main/java/com/graps/remindme/ui/base/BaseAdapter.kt
@@ -1,24 +1,24 @@
 package com.graps.remindme.ui.base
 
-import android.os.Bundle
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.databinding.ViewDataBinding
 import androidx.databinding.library.baseAdapters.BR
-import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 
-
-abstract class BaseAdapter<T>(private var items: List<T>, private val listener:BaseInteractionListener) :
+abstract class BaseAdapter<T>(
+    private var items: List<T>,
+    private val listener: BaseInteractionListener
+) :
     RecyclerView.Adapter<BaseAdapter.BaseViewHolder>() {
 
     abstract val layoutId: Int
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): BaseViewHolder {
         return ItemViewHolder(
-            DataBindingUtil.inflate(LayoutInflater.from(parent.context),
+            DataBindingUtil.inflate(
+                LayoutInflater.from(parent.context),
                 layoutId,
                 parent,
                 false
@@ -28,8 +28,8 @@ abstract class BaseAdapter<T>(private var items: List<T>, private val listener:B
 
     override fun onBindViewHolder(holder: BaseViewHolder, position: Int) {
         val currentItem = items[position]
-        when(holder) {
-            is ItemViewHolder ->{
+        when (holder) {
+            is ItemViewHolder -> {
                 holder.binding.setVariable(BR.item, currentItem)
                 //holder.binding.setVariable(BR.listener, listener)
             }
@@ -49,7 +49,7 @@ abstract class BaseAdapter<T>(private var items: List<T>, private val listener:B
 
     class ItemViewHolder(val binding: ViewDataBinding) : BaseViewHolder(binding)
 
-    abstract class BaseViewHolder(binding : ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
+    abstract class BaseViewHolder(binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root)
 
     interface BaseInteractionListener
 }
--- app/src/main/java/com/graps/remindme/ui/base/ReminderDiffutils.kt
+++ app/src/main/java/com/graps/remindme/ui/base/ReminderDiffutils.kt
@@ -2,14 +2,17 @@ package com.graps.remindme.ui.base
 
 import androidx.recyclerview.widget.DiffUtil
 
-class ReminderDiffutils<T>(private val remindersList: List<T>, private val remindersNewList: List<T>) :
+class ReminderDiffutils<T>(
+    private val remindersList: List<T>,
+    private val remindersNewList: List<T>
+) :
     DiffUtil.Callback() {
-        override fun getOldListSize() = remindersList.size
+    override fun getOldListSize() = remindersList.size
 
-        override fun getNewListSize() = remindersNewList.size
+    override fun getNewListSize() = remindersNewList.size
 
-        override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
-            remindersList[oldItemPosition] == remindersNewList[newItemPosition]
+    override fun areItemsTheSame(oldItemPosition: Int, newItemPosition: Int) =
+        remindersList[oldItemPosition] == remindersNewList[newItemPosition]
 
-        override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
+    override fun areContentsTheSame(oldItemPosition: Int, newItemPosition: Int) = true
 }
