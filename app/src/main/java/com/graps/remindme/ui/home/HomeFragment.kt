eb4f811b78f9cc1a180f8778a0b0ab78f6a82fba
Amnah Ali <amnah1li@gmail.com>
2021-11-25 15:55:39 +0300

    active floating button in home fragment

--- app/src/main/java/com/graps/remindme/ui/home/HomeFragment.kt
+++ app/src/main/java/com/graps/remindme/ui/home/HomeFragment.kt
@@ -1,9 +1,11 @@
 package com.graps.remindme.ui.home
 
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.activityViewModels
+import androidx.navigation.fragment.findNavController
 import com.graps.remindme.R
 import com.graps.remindme.data.database.ReminderDatabase
 import com.graps.remindme.databinding.FragmentHomeBinding
@@ -11,7 +13,7 @@ import com.graps.remindme.ui.base.BaseFragment
 import com.vivekkaushik.datepicker.OnDateSelectedListener
 import java.util.*
 
-class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
+class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) , View.OnClickListener{
     override val viewModel by activityViewModels<HomeViewModel>()
     override val bindingInflater: (LayoutInflater, Int, ViewGroup?, Boolean) ->
     FragmentHomeBinding = DataBindingUtil::inflate
@@ -36,9 +38,13 @@ class HomeFragment : BaseFragment<FragmentHomeBinding>(R.layout.fragment_home) {
 
             })
 
-            val dates: Array<Date> = arrayOf<Date>(Calendar.getInstance().getTime())
+            val dates: Array<Date> = arrayOf<Date>(Calendar.getInstance().time)
             binding.datePickerTimeline.deactivateDates(dates)
-
         }
+        binding.addFab.setOnClickListener(this)
+    }
+
+    override fun onClick(p0: View?) {
+        findNavController().navigate(R.id.action_homeFragment_to_intakeFragment)
     }
 }
