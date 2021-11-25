daa20b638e16429c2384e336d3b5a3ef7d618833
Amnah Ali <amnah1li@gmail.com>
2021-11-25 14:22:18 +0300

    remove instance line in main activity

--- app/src/main/java/com/graps/remindme/ui/RemindMeActivity.kt
+++ app/src/main/java/com/graps/remindme/ui/RemindMeActivity.kt
@@ -2,15 +2,16 @@ package com.graps.remindme.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
 import com.graps.remindme.R
 import com.graps.remindme.data.database.ReminderDatabase
+import com.graps.remindme.databinding.ActivityRemindMeBinding
 
 class RemindMeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityRemindMeBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_remind_me)
-
-//        ReminderDatabase.getInstance(applicationContext)
 
     }
 }
