da0228a8ff4864689c34ee48301b9311c37861a2
Rafal A <rafal.akeel99@gmail.com>
2021-11-26 23:28:59 +0300

    active done in intake fragment

--- app/src/main/java/com/graps/remindme/ui/inTake/IntakeViewModel.kt
+++ app/src/main/java/com/graps/remindme/ui/inTake/IntakeViewModel.kt
@@ -1,12 +1,18 @@
 package com.graps.remindme.ui.inTake
 
+import android.app.Activity
+import android.content.Context
+import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.MutableLiveData
+import com.google.android.material.internal.ContextUtils.getActivity
 import com.graps.remindme.data.Reminder
 import com.graps.remindme.data.repository.ReminderRepository
 import com.graps.remindme.ui.base.BaseViewModel
 import io.reactivex.rxjava3.core.Scheduler
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.schedulers.Schedulers.io
+import kotlin.coroutines.coroutineContext
 
 class IntakeViewModel: BaseViewModel() {
 
@@ -24,14 +30,31 @@ class IntakeViewModel: BaseViewModel() {
     private val  repository = ReminderRepository()
 
     init {
-        amount.postValue(1)
+        amount.value=1
     }
     fun addReminder()
     {
         pillsName.value.let {
-            repository.insertReminder(Reminder(id = 0, name = it,amount.value,dose.value,year = year.value, month.value, day = day.value,frequency.value,"1",phoneNo.value))
+            repository.insertReminder(Reminder(id = null,
+                name = it,
+                amount.value,
+                dose.value,
+                year = year.value,
+                month.value,
+                day = day.value,
+                frequency.value,
+                "1",
+                phoneNo.value)
+            )
                 ?.subscribeOn(io())
-                ?.subscribe()
+                ?.subscribe(::onSuccess,::onError)
         }
     }
+
+    private fun onSuccess() = amount.postValue(1)
+
+    private fun onError(e:Throwable){
+      println(e.message)
+    }
+
 }
