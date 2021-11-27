44f284a8f8cc267fce72eacd244f2a20fec1e3dc
Rafal A <rafal.akeel99@gmail.com>
2021-11-27 22:01:55 +0300

    add appearance name

--- app/src/main/java/com/graps/remindme/util/Appearance.kt
+++ app/src/main/java/com/graps/remindme/util/Appearance.kt
@@ -3,4 +3,5 @@ package com.graps.remindme.util
 data class Appearance(
     val image: Int,
     val id:Int,
-)
+    val appearanceText:String,
+    )
--- app/src/main/java/com/graps/remindme/util/AppearanceList.kt
+++ app/src/main/java/com/graps/remindme/util/AppearanceList.kt
@@ -5,9 +5,9 @@ import com.graps.remindme.R
 class AppearanceList {
 
     val appearanceList = listOf<Appearance>(
-        Appearance(R.drawable.ic_tablets, 1),
-        Appearance(R.drawable.ic_injection,2),
-        Appearance(R.drawable.ic_capsule1,3),
-        Appearance(R.drawable.ic_spray,4),
+        Appearance(R.drawable.ic_tablets, 1,"Tablets"),
+        Appearance(R.drawable.ic_injection,2,"Injection"),
+        Appearance(R.drawable.ic_capsule1,3,"Capsule"),
+        Appearance(R.drawable.ic_spray,4,"Spray"),
     )
 }
